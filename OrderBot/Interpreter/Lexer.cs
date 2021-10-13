85a0ec84e78344d6aa45f0094288302d50c5314f
Stephen Smith <mail@sasmith.ca>
2021-10-12 21:00:53 -0400

    complete basic lexing with some tests

--- OrderBot/Interpreter/Lexer.cs
+++ OrderBot/Interpreter/Lexer.cs
@@ -1,7 +1,59 @@
+using System.Collections.Generic;
+using System.Linq;
+
 namespace PizzaBot.Interpretation
 {
   internal class Lexer
   {
-    
+    static string[] toppings = 
+    {
+      "pepperoni", "sausage", "bacon", "chicken", "ham", "beef", "steak", "salami",
+      "onion", "mushrooms", "peppers", "pineapple", "olives", "tomatoes", "spinach",
+      "jalapenos", "provolone", "cheese", "cheddar"
+    };
+    static string[] bases = 
+    {
+      "tomato", "marinara", "white", "alfredo"
+    };
+    static string[] pizzas =
+    {
+     "cheese", "pepperoni", "deluxe", "hawaiian", "veggie", "canadian",
+      "meat", 
+    };
+
+    static string[] grammar = 
+    {
+      "a", "an", "with", "without", "and", "or", "no", "pizza", "half"
+    };
+    public static List<Token> scan(string src)
+    {
+      List<Token> ret = new List<Token>();
+
+      foreach (string s in src.Split(" "))
+      {
+        ret.Add(Tokenise(s.ToLower()));
+      }
+      
+      return ret;
+    }
+
+    /*
+      Converts a string into a Token based on the table at the top of the file.
+    */
+    private static Token Tokenise(string input) => input switch
+    {
+      _ when toppings.Contains(input) => new Token(TokenType.TOPPING, input),
+      _ when bases.Contains(input) => new Token(TokenType.BASE, input),
+      _ when pizzas.Contains(input) => new Token(TokenType.PIZZA, input),
+      _ when grammar.Contains(input) => new Token(TokenType.GRAMMAR, input),
+      _ when isInt(input) => new Token(TokenType.NUMBER, input),
+      _ => new Token(TokenType.UNKNOWN, input)
+     };
+
+     private static bool isInt(string s)
+     {
+       int x;
+       return (int.TryParse(s, out x));
+     }
   }
 }
--- OrderBot/Interpreter/Token.cs
+++ OrderBot/Interpreter/Token.cs
@@ -1,7 +1,40 @@
+using System.Runtime.CompilerServices;
+[assembly:InternalsVisibleTo("OrderBot.tests")]
 namespace PizzaBot.Interpretation 
 {
+  /*
+    A token is the fundemental unit of our grammar. A token has a type,
+    and some underlying value.
+  */
   internal class Token
   {
+    internal TokenType type;
+    internal string value;
     
+    public Token(TokenType type, string value)
+    {
+      this.type = type; 
+      this.value = value;
+    }
+
+    // this override is primarily for testing purposes. the parser will never 
+    // need to do string comparisons.
+    public override bool Equals(object obj)
+    {
+      if ((obj == null) || ! this.GetType().Equals(obj.GetType()))
+      {
+         return false;
+      }
+      else
+      {
+        Token t = (Token) obj;
+        return this.type == t.type && this.value == t.value;
+      }
+    }
+
+    public override int GetHashCode()
+    {
+      return base.GetHashCode();
+    }
   }
 }
